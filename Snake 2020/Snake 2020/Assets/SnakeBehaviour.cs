4237830e83ebced5f73a635931e13be4704bc0f0
sviatkk <sviatik1208@gmail.com>
2019-10-01 23:37:57 +0300

    Added walk and atack animation

--- Snake 2020/Snake 2020/Assets/SnakeBehaviour.cs
+++ Snake 2020/Snake 2020/Assets/SnakeBehaviour.cs
@@ -3,40 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SnakeBehaviour : MonoBehaviour
-{
+{       Animator m_animator;
     // Start is called before the first frame update
     void Start()
     {
-        print("SNAKEEEEE");
+        m_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.z--;
-            this.transform.position = position;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.z++;
-            this.transform.position = position;
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.z++;
-            this.transform.position = position;
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.z--;
-            this.transform.position = position;
-        }
+        bool isWalkingPressed = Input.GetKey("w");
+        m_animator.SetBool("isWalking",isWalkingPressed);
 
-    }
+         bool isAtackPressed = Input.GetKey("space");
+        m_animator.SetBool("isAtacking",isAtackPressed);
+}
 }

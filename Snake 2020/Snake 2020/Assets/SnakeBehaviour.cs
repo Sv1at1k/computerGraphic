13def89c457c03db9998ab1e47af09dcb4d1dcd5
sviatkk <sviatik1208@gmail.com>
2019-10-02 22:29:34 +0300

    Snake now moving

--- Snake 2020/Snake 2020/Assets/SnakeBehaviour.cs
+++ Snake 2020/Snake 2020/Assets/SnakeBehaviour.cs
@@ -2,53 +2,86 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.AI;
+
+
 public class SnakeBehaviour : MonoBehaviour
-{       Animator m_animator;
-    // Start is called before the first frame update
+{
+
+    Animator m_animator;
+    public float movementSpeed = 20.0f;
+    public float rotSpeed = 45.0f;
+    public float speed = 0;
+
+    private float moveSpeed = 1;
     void Start()
     {
         m_animator = GetComponent<Animator>();
+
     }
 
-    // Update is called once per frame
     void Update()
+    {
+
+
+
+
+
+
+        move();
+        moveAnimation();
+
+
+
+
+    }
+    private void move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -1, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+
+    }
+    private void moveAnimation()
     {
         bool isWalkingPressed = Input.GetKey("w");
         bool isLeftPressed = Input.GetKey("a");
         bool isRightPressed = Input.GetKey("d");
         bool isAtackPressed = Input.GetKey("space");
-       
-    
-        if(isWalkingPressed || isLeftPressed ||isRightPressed){
-                 m_animator.SetBool("isWalking",true);
-        }else {
 
-             m_animator.SetBool("isWalking",false);
-        }
-      
-        if (isWalkingPressed)
+
+        if (isWalkingPressed || isLeftPressed || isRightPressed)
         {
-           
-           
+            m_animator.SetBool("isWalking", true);
         }
-        if (isLeftPressed)
+        else
         {
-           
-           
-        }
-        if (isRightPressed)
-        {   
-            
-            
-             
+            m_animator.SetBool("isWalking", false);
         }
         if (isAtackPressed)
-        { m_animator.SetBool("isAtacking",true);
-
-        }else {
-              m_animator.SetBool("isAtacking",false);
+        {
+            m_animator.SetBool("isAtacking", true);
 
         }
-       
-}
+        else
+        {
+            m_animator.SetBool("isAtacking", false);
+
+        }
+
+
+
+    }
+
 }

165277392528315f5e8da187e1b8d81182671dff
sviatkk <sviatik1208@gmail.com>
2019-10-10 00:16:11 +0300

    Lab3

---
+++ Snake 2020/Snake 2020/Assets/SnakeHome.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeHome : MonoBehaviour
+{
+      void OnCollisionEnter(Collision colider)
+    {
+        var fullname =colider.gameObject.name;
+              
+        if (name == "Znake")
+        {  Debug.Log("+1");
+          
+            //m_animator.SetBool("isDead", true);
+           //   isDead = true;
+
+        }
+    }
+}

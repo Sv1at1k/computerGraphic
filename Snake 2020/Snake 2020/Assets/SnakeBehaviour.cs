04133017737892c1d59785fe69ca9a9456a84b5f
sviatkk <sviatik1208@gmail.com>
2019-10-10 00:08:56 +0300

    Added lifeBar

--- Snake 2020/Snake 2020/Assets/SnakeBehaviour.cs
+++ Snake 2020/Snake 2020/Assets/SnakeBehaviour.cs
@@ -3,73 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 
 public class SnakeBehaviour : MonoBehaviour
 {
     Animator m_animator;
-
-
     private bool isGameOver;
     private bool isDead;
     bool isWalkingPressed;
     bool isLeftPressed;
     bool isRightPressed;
     bool isAtackPressed;
-
-   
-
     float speed = 1;
+    int lifes = 2;
+
+    private RawImage heart1, heart2, heart3;
 
     public Rigidbody rb;
 
-     void OnCollisionEnter(Collision colider)
+    void OnCollisionEnter(Collision colider)
     {
-        var fullname =colider.gameObject.name;
-        var  name = fullname.Substring(0, 4);
-       
+        var fullname = colider.gameObject.name;
+        var name = fullname.Substring(0, 4);
+
         if (name == "Cube")
-        {  
-          
-            m_animator.SetBool("isDead", true);
-              isDead = true;
+        {
+            lifes--;
+            if (lifes < 1)
+            {
+                m_animator.SetBool("isDead", true);
+                isDead = true;
+            }
+            Debug.Log(lifes);
 
         }
+        else if (name == "Plan" & lifes < 3)
+        {
+            Destroy(colider.gameObject);
+            lifes++;
+            Debug.Log(lifes);
+        }
     }
+
     void Start()
     {   var boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = true;
         rb = GetComponent<Rigidbody>();
         m_animator = GetComponent<Animator>();
 
+        heart1 = GameObject.Find("heart1").GetComponent<RawImage>();
+        heart2 = GameObject.Find("heart2").GetComponent<RawImage>();
+        heart3 = GameObject.Find("heart3").GetComponent<RawImage>();
+
     }
 
     void FixedUpdate()
-    {   
-        // float moveHoriontal = Input.GetAxis("Horizontal");
-        // float moveVertical = Input.GetAxis("Vertical");
-        // Vector3 movement = new Vector3(moveHoriontal, 0, moveVertical);
-        // rb.AddForce(movement * speed);
-       if(!isDead){
-        moveAnimation();
-        move();
-       }
+    {
+        if (!isDead)
+        {
+
+            moveAnimation();
+            move();
+        }
+    }
+
+    void Update(){
+        getLifes();
+
     }
 
     private void move()
-    {
+    {   
 
         Vector3 movement = new Vector3();
 
         if (isWalkingPressed)
         {
-           // movement = new Vector3(0, 0, 10);
             transform.position += transform.forward * speed;
-            rb.MovePosition(transform.position  * Time.fixedDeltaTime);
-
-
+            rb.MovePosition(transform.position * Time.fixedDeltaTime);
         }
-
         if (isRightPressed)
         {
             movement = new Vector3(0, 79, 0);
@@ -118,6 +131,37 @@ public class SnakeBehaviour : MonoBehaviour
 
     private void endGame()
     {
+
+
+
+    }
+    private void getLifes()
+    {   
+        switch (lifes)
+        {
+            case 3:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(true);
+                break;
+            case 2:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(true);
+                heart3.gameObject.SetActive(false);
+                break;
+            case 1:
+                heart1.gameObject.SetActive(true);
+                heart2.gameObject.SetActive(false);
+                heart3.gameObject.SetActive(false);
+                break;
+            case 0:
+                heart1.gameObject.SetActive(false);
+                heart2.gameObject.SetActive(false);
+                heart3.gameObject.SetActive(false);
+                break;
+
+
+        }
 
 
 
